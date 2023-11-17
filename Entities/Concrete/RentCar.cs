bb76ea93cc66ff2f943e96836b1a3f821bacafb9
Busra <Busra@DESKTOP-BCNGC9H>
2023-11-17 12:53:11 +0300

    Add project files.

---
+++ Entities/Concrete/RentCar.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class RentCar : IEntity
+    {
+        public int Id{ get; set; }
+        public string CarId { get; set; }
+        public string CustomerId { get; set; }
+        public string EmployeeId { get; set; }
+        public DateTime RentDate { get; set; }
+
+
+    }
+}
